53c9495363564c99c86fcbe6e875b41080a387c7
Juan Ignacio Sackmann Sala <jsackmann@itba.edu.ar>
2022-09-27 17:37:50 -0300

    changed to convert method and added gb and fasta format to gitignore

--- scripts/Ej1.py
+++ scripts/Ej1.py
@@ -5,21 +5,31 @@ import sys
 from Bio import SeqIO
 
 
-
+#check valid arguments
 n = len(sys.argv)
 if n != 2:
     print("Error: Total arguments passed:",n-1,"Was expecting 1")
     exit(1)
- 
+
+#Extracting the name of the file
 input_path=sys.argv[1]
 path=os.path.dirname(input_path)
 filename=os.path.basename(input_path)
 name, ext = os.path.splitext(filename)
 
+if not os.path.isfile(input_path):
+    print ("File doesnt exist/couldn't be opened")
+    exit(1)
 
-with open(input_path, "r") as input_handle:
-    with open(name+".fasta", "w") as output_handle:
-        sequences = SeqIO.parse(input_handle, "genbank")
-        count = SeqIO.write(sequences, output_handle, "fasta")
+if not os.access(input_path, os.R_OK):
+    print ("File not readable")
+    exit(1)
 
-print("Converted %i records" % count)
+
+
+
+#converting the file to fasta
+
+result_count=SeqIO.convert(input_path, "genbank", name+".fasta", "fasta")
+
+print("Converted %i records" % result_count)
